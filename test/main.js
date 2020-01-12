57dad779965352f29596b4d02d5660f5455331b1
Phil Deschaine <phil@hmlet.com>
2020-01-12 20:05:02 +0800

    working on tests

--- test/main.js
+++ test/main.js
@@ -8,20 +8,45 @@ import { pullTable } from '../src/airtable'
 
 import { auth_key, base_name } from './fixtures/config'
 
-test("can pull correctly", async(t) => {
+import path from 'path'
+import util from 'util'
+import fs from 'fs'
+
+const readFile = util.promisify(fs.readFile)
+
+test.beforeEach(async(t) => {
+  const primary = "Songs1"
+  const view = "Main"
+
   nock('https://api.airtable.com:443', { encodedQueryParams: true })
-    .get(`/v0/${base_name}/Songs`)
-    .query({ view: "Main" })
+    .get(`/v0/${base_name}/${primary}`)
+    .query({ view })
     .reply(200, songs_response, headers)
 
-  t.context.songs = await pullTable({
+  await pullTable({
     auth_key,
     base_name,
-    primary: "Songs",
-    view: "Main",
+    primary,
+    view
   })
 
-  t.pass()
+  const filename = path.resolve(`${__dirname}/../build/${primary}.json`)
+  const file_contents = await readFile(filename)
+  t.context.data = JSON.parse(file_contents)
+})
 
+test("ids pull correctly", (t) => {
+  t.is(t.context.data[0].__id, songs_response.records[0].id)
+  t.is(t.context.data[1].__id, songs_response.records[1].id)
+  t.is(t.context.data[2].__id, songs_response.records[2].id)
+})
 
+test("fields pull correctly", (t) => {
+  t.is(t.context.data[0].URL, songs_response.records[0].fields.URL)
+  t.is(t.context.data[1].URL, songs_response.records[1].fields.URL)
+  t.is(t.context.data[2].URL, songs_response.records[2].fields.URL)
+})
+
+test("right number of records", (t) => {
+  t.is(t.context.data.length, songs_response.records.length)
 })
