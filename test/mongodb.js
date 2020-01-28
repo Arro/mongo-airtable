a4932086913c359f66ff3d8f36ce8d8c3d03b74d
Phil Deschaine <phil@hmlet.com>
2020-01-28 10:43:11 +0800

    more test changes

--- test/mongodb.js
+++ test/mongodb.js
@@ -28,43 +28,30 @@ test.beforeEach(async(t) => {
   await db.collection("furniture").removeMany({})
   await db.collection("books").removeMany({})
 
-  const f_fixture_filename = path.resolve(`${__dirname}/../test/fixtures/airtable-transformed/000_furniture.json`)
-  const f_fixture_text = await readFile(f_fixture_filename)
-  const f_fixture = JSON.parse(f_fixture_text)
+  let fixtures = {
+    f: 'airtable-transformed/000_furniture.json',
+    b: 'airtable-transformed/001_books.json',
+    f2: 'airtable-transformed/002_furniture_transformed.json',
+    f3: 'airtable-transformed/003_furniture_diff.json',
+    config: 'config-yaml/000_furniture_and_books.yaml',
+    config2: 'config-yaml/001_furniture.yaml'
+  }
+  let fixture_texts = {}
 
-  const b_fixture_filename = path.resolve(`${__dirname}/../test/fixtures/airtable-transformed/001_books.json`)
-  const b_fixture_text = await readFile(b_fixture_filename)
-  const b_fixture = JSON.parse(b_fixture_text)
-
-  const f2_fixture_filename = path.resolve(`${__dirname}/../test/fixtures/airtable-transformed/002_furniture_transformed.json`)
-  const f2_fixture_text = await readFile(f2_fixture_filename)
-  const f2_fixture = JSON.parse(f2_fixture_text)
-
-  const f3_fixture_filename = path.resolve(`${__dirname}/../test/fixtures/airtable-transformed/003_furniture_diff.json`)
-  const f3_fixture_text = await readFile(f3_fixture_filename)
-  const f3_fixture = JSON.parse(f3_fixture_text)
-
-  const config_filename = path.resolve(`${__dirname}/../test/fixtures/config-yaml/000_furniture_and_books.yaml`)
-  const config_text = await readFile(config_filename, 'utf-8')
-  const config = yaml.parse(config_text)
-
-  const config2_filename = path.resolve(`${__dirname}/../test/fixtures/config-yaml/001_furniture.yaml`)
-  const config2_text = await readFile(config2_filename, 'utf-8')
-  const config2 = yaml.parse(config2_text)
-
+  for (const name in fixtures) {
+    const filepath = fixtures[name]
+    const fixture_filename = path.resolve(`${__dirname}/../test/fixtures/${filepath}`)
+    fixture_texts[name] = await readFile(fixture_filename, 'utf-8')
+    if (filepath.endsWith('json')) fixtures[name] = JSON.parse(fixture_texts[name])
+    if (filepath.endsWith('yaml')) fixtures[name] = yaml.parse(fixture_texts[name])
+  }
+  
   t.context = {
     db,
     connection,
-    f_fixture_text,
-    f_fixture,
-    b_fixture_text,
-    b_fixture,
-    f2_fixture_text,
-    f2_fixture,
-    f3_fixture,
+    fixtures,
+    fixture_texts,
     output_dirname,
-    config,
-    config2
   }
 })
 
@@ -73,10 +60,10 @@ test.afterEach(async(t) => {
 })
 
 test.serial("it puts into db correctly", async(t) => {
-  const { output_dirname, f_fixture_text, f_fixture, db } = t.context
+  const { output_dirname, fixture_texts, fixtures, db } = t.context
 
   const output_filename = path.resolve(`${output_dirname}/Furniture.json`)
-  await writeFile(output_filename, f_fixture_text, 'utf-8')
+  await writeFile(output_filename, fixture_texts.f, 'utf-8')
 
   await putIntoDB({
     primary: "Furniture",
@@ -88,20 +75,20 @@ test.serial("it puts into db correctly", async(t) => {
   let furniture_in_mongo = await collection.find().toArray()
   furniture_in_mongo.forEach(f => delete f._id)
 
-  t.deepEqual(furniture_in_mongo, f_fixture)
+  t.deepEqual(furniture_in_mongo, fixtures.f)
 })
 
 
 test.serial("can loop through config tables", async(t) => {
-  const { output_dirname, f_fixture, f_fixture_text, b_fixture, b_fixture_text, config, db } = t.context
+  const { output_dirname, fixtures, fixture_texts, db } = t.context
 
   const f_output_filename = path.resolve(`${output_dirname}/Furniture.json`)
-  await writeFile(f_output_filename, f_fixture_text, 'utf-8')
+  await writeFile(f_output_filename, fixture_texts.f, 'utf-8')
 
   const b_output_filename = path.resolve(`${output_dirname}/Books.json`)
-  await writeFile(b_output_filename, b_fixture_text, 'utf-8')
+  await writeFile(b_output_filename, fixture_texts.b, 'utf-8')
 
-  await initialInsert(config)
+  await initialInsert(fixtures.config)
 
   const f_collection = db.collection("furniture")
   let furniture_in_mongo = await f_collection.find().toArray()
@@ -111,27 +98,26 @@ test.serial("can loop through config tables", async(t) => {
   let books_in_mongo = await b_collection.find().toArray()
   books_in_mongo.forEach(b => delete b._id)
 
-  t.deepEqual(furniture_in_mongo, f_fixture)
-  t.deepEqual(books_in_mongo, b_fixture)
+  t.deepEqual(furniture_in_mongo, fixtures.f)
+  t.deepEqual(books_in_mongo, fixtures.b)
 })
 
 
 test.serial("can check for new ones", async(t) => {
-  const { output_dirname, f_fixture_text, f2_fixture, db, config2, f3_fixture } = t.context
+  const { output_dirname, fixture_texts, fixtures, db } = t.context
 
   const output_filename = path.resolve(`${output_dirname}/Furniture.json`)
-  await writeFile(output_filename, f_fixture_text, 'utf-8')
+  await writeFile(output_filename, fixture_texts.f, 'utf-8')
 
   const collection = db.collection("furniture")
-  await collection.insertMany(f2_fixture)
+  await collection.insertMany(fixtures.f2)
 
-  await lookForChanges(config2)
+  await lookForChanges(fixtures.config2)
 
   const diff_filename = path.resolve(`${output_dirname}/Furniture_diff.json`)
   const diff_text = await readFile(diff_filename, 'utf-8')
   const diffs = JSON.parse(diff_text)
 
-  t.deepEqual(diffs, f3_fixture)
-
+  t.deepEqual(diffs, fixtures.f3)
 })
 
