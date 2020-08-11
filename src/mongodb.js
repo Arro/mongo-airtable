77e9da6222d44c59a2d0752977506ed53d7bd9a4
Gaëtan Cottrez <gaetan@passiverecords.com>
2020-08-11 15:31:50 +0200

    :bug: fix bug on insertMany with empty airtable

--- src/mongodb.js
+++ src/mongodb.js
@@ -22,7 +22,8 @@ export async function putIntoDB({ primary, collection, database }) {
 
   await collection.removeMany({})
 
-  await collection.insertMany(data)
+  if(data.length > 0)
+    await collection.insertMany(data)
 
   await connection.close()
 }
