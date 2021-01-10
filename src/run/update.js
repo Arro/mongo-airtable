90c2edc78cc92208eb80eeca479850c7aaffff6a
Phil Deschaine <deschai5@gmail.com>
2021-01-10 16:23:09 +0800

    cleanup

--- src/run/update.js
+++ src/run/update.js
@@ -1,22 +1,18 @@
-import path from 'path'
-import util from 'util'
-import fs from 'fs'
-import yaml from 'yaml'
+import path from "path"
+import util from "util"
+import fs from "fs"
+import dotenv from "dotenv"
 
-import { seeWhatChanged } from '../merge'
-import dotenv from 'dotenv'
+import { seeWhatChanged } from "../merge"
 
 dotenv.config()
 
 const readFile = util.promisify(fs.readFile)
 
-const run = async() => {
+const run = async () => {
   const filename = path.resolve(process.env.PATH_MONGO_AIRTABLE_YAML)
-  let config = await readFile(filename, 'utf-8')
-  config  = yaml.parse(config)
-
+  let config = await readFile(filename, "utf-8")
   await seeWhatChanged(config)
 }
 
 run()
-
